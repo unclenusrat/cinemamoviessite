08c22939d369ce3b991aa5dca6c891d260ffecb0
unclenusrat <134220810+unclenusrat@users.noreply.github.com>
2023-06-08 18:39:52 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://c`
+					`https://cinemamovies.site/watch-now/`
 				}`,
 			},
 		};
