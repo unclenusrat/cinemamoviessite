1dda13e10018cdb2154215e3280648be07c1b263
unclenusrat <134220810+unclenusrat@users.noreply.github.com>
2023-06-10 03:36:56 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://cinemamovies.site/see-more/`
+					`https://relaxingfacts.blogspot.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
